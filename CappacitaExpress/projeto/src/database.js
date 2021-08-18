a8b2f90ab2cb48e0ad97eaad49df99824042f497
vania santos <wanhya2@hotmail.com>
2021-08-18 16:31:10 -0300

    batalha pokemon

--- CappacitaExpress/projeto/src/database.js
+++ CappacitaExpress/projeto/src/database.js
@@ -2,7 +2,7 @@ const sequence ={
     _id: 1,
     get id() { return this._id++}
 }
-const pokemons ={}
+const pokemons =[]
 function salvarpokemons(pokemon){
     if (!pokemon.id) pokemon.id = sequence.id
     pokemons[pokemon.id] = pokemon
@@ -16,4 +16,56 @@ function mostrarpokemon(id){
 function mostrarpokemons() {
     return Object.values(pokemons)
 }
-module.exports = { salvarpokemons, mostrarpokemon, mostrarpokemons}
+
+function atualizarpokemon(id, pokemon) {
+    pokemons[id] = pokemon
+    return pokemon
+}
+
+function deletarpokemon(id) {
+    sequence._id = sequence._id -1
+    const pokemondeletado = pokemons[1]
+    pokemons.splice(id, 1)
+    pokemons.forEach(pokemon => {
+        if(pokemon.id > id) {
+            pokemon.id = pokemon.id - 1
+        }
+    })
+    return pokemondeletado
+}
+
+function batalhapokemon(id1, id2){
+    const superefetivo = 40
+    const efetivo = 20
+    const naoefetivo = 10
+    
+    const pokemon1 = pokemons[id1]
+    const pokemon2 = pokemons[id2]
+
+    if(pokemon1.hp != 0 && pokemon2.hp != 0) {
+        if(pokemon1.tipo == pokemon2.fraqueza) {
+            pokemon2.hp = pokemon2.hp - superefetivo
+            } else if(pokemon1.tipo == pokemon2.resistencia){
+                pokemon2.hp = pokemon2.hp - naoefetivo
+            } else {
+                pokemon2.hp = pokemon2.hp - efetivo
+            }
+    }
+
+    if(pokemon2.hp != 0 && pokemon1.hp != 0) {
+        if(pokemon2.tipo == pokemon1.fraqueza) {
+            pokemon1.hp = pokemon1.hp - superefetivo
+            } else if(pokemon2.tipo == pokemon1.resistencia){
+                pokemon1.hp = pokemon1.hp - naoefetivo
+            } else {
+                pokemon1.hp = pokemon1.hp - efetivo
+            }
+    }
+
+    if(pokemon1.hp < 0) pokemon1.hp = 0
+    if(pokemon2.hp < 0)  pokemon2.hp = 0
+    return `${pokemon1.nome}: ${pokemon1.hp} / ${pokemon2.nome}: ${pokemon2.hp}`
+
+}
+
+module.exports = { salvarpokemons, mostrarpokemon, mostrarpokemons, atualizarpokemon, deletarpokemon, batalhapokemon}
--- CappacitaExpress/projeto/src/server.js
+++ CappacitaExpress/projeto/src/server.js
@@ -16,9 +16,30 @@ app.get('/pokemons/:id', (req, res) => {
 app.post('/pokemons', (req, res) => {
     const pokemon = database.salvarpokemons ({
         nome: req.body.nome,
-        tipo: req.body.tipo
+        tipo: req.body.tipo,
+        fraqueza: req.body.fraqueza,
+        resistencia: req.body.resistencia,
+        hp: 100
     })
     res.send(pokemon)
 })
+
+app.put('/pokemons/:id', (req, res) => {
+    const pokemon = database.atualizarpokemon(req.params.id, {
+        nome: req.body.nome,
+        tipo: req.body.tipo,
+        id: parseInt(req.params.id)
+    })
+    res.send(pokemon)
+})
+
+app.delete('/pokemons/:id', (req, res) => {
+    res.send(database.deletarpokemon(req.params.id))
+})
+
+app.post ('/batalha', (req, res) => {
+    res.send(database.batalhapokemon(req.body.id1, req.body.id2))
+})
+
 
 app.listen(3003)
